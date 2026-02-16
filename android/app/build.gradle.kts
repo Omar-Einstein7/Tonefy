10db58004e84b597ceae49f7cea86e84e1690036
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-02-17 00:37:41 +0200

    build: update Android and Dart SDK dependencies
    - Upgrade Gradle wrapper from 8.12 to 8.14
    - Update Android Gradle plugin from 8.7.3 to 8.11.1
    - Update Kotlin plugin from 2.1.0 to 2.2.20
    - Bump Dart SDK constraint from ^3.8.1 to ^3.10.8
    - Update flutter_lints from ^5.0.0 to ^6.0.0
    - Explicitly set compileSdk and targetSdk to 36
    - Update various transitive dependencies to latest versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.flutter_template"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -16,7 +16,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+         jvmTarget = JavaVersion.VERSION_17.toString()
     }
      kotlin {
         jvmToolchain(17)
@@ -28,7 +28,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
